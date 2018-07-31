a8942528a9d0803cd72e452c75a46b6356e4e9b9
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2018-07-30 17:39:01 -0700

    use facetting instead of for loop

--- src/2018-07-27_rgnl_clean-eol-data.R
+++ src/2018-07-27_rgnl_clean-eol-data.R
@@ -12,6 +12,8 @@ library("magrittr")
 library("ggpubr")
 
 # todo: ----------------
+# > add acute.deaths lines in the graphs, by COC 
+# > rename col stl.decomp to deaths.stl; add new col acutedeaths.stl 
 
 
 # read in data: ----------------
@@ -49,12 +51,17 @@ deaths.data %<>%
       mutate(deaths.ts = map(data, extract_deaths)) %>% 
       mutate(stl.decomp = map(deaths.ts, function(x) {
                   stl <- stl(x, s.window = "periodic")
-                  return(stl[[1]])
+                  
+                  stl.df <- stl[[1]] %>% as.data.frame() %>% 
+                        mutate(data = seasonal + trend + remainder, 
+                               timeperiod = seq_along(seasonal))
+                  
+                  return(stl.df)
                   }
             ))
 
 # result: 
-deaths.data$stl.decomp[[1]]
+deaths.data$stl.decomp[[5]]
 
 
 
@@ -69,44 +76,58 @@ deaths.data$stl.decomp[[1]]
 #             geom_line()
 # 
 
-
-# use for loop to plot each COC: 
-plots <- list()
-
-# names for plot titles: 
-coc.list <- unique(deaths.data$area)
-
-# define plotting function: 
-plot.fn <- function(df){
-      df %>% 
-      mutate(data = seasonal + trend + remainder, 
-              timeperiod = seq_along(seasonal)) %>% 
+unnest(deaths.data, stl.decomp) %>% 
+      as.data.frame() %>% 
+      filter(!is.na(area)) %>% 
       ggplot(aes(x = timeperiod, 
                  y = trend)) + 
-      geom_line(colour = "dodgerblue") + 
-      scale_y_continuous(limits = c(0, 1000)) + 
-      labs(title = paste0(coc.list[i])) + 
+      geom_line() + 
+      facet_wrap(~area) + 
       theme_classic()
-}
-
-
-# apply fn in loop: 
-for (i in 1:5){
-      plots[[i]] <- 
-      deaths.data$stl.decomp[[i]] %>% 
-      as.data.frame() %>% 
-      plot.fn() 
       
-}
+      
+      
 
-# view plots: 
-print(plots)
-plots[[5]]
 
-ggarrange(plots[[2]], 
-          plots[[3]], 
-          plots[[4]], 
-          plots[[5]])
-# todo: why not just facet them? 
+
+
+
+# > alternative approach: use for loop to plot each COC: 
+# plots <- list()
+# 
+# # names for plot titles: 
+# coc.list <- unique(deaths.data$area)
+# 
+# # define plotting function: 
+# plot.fn <- function(df){
+#       df %>% 
+#       mutate(data = seasonal + trend + remainder, 
+#               timeperiod = seq_along(seasonal)) %>% 
+#       ggplot(aes(x = timeperiod, 
+#                  y = trend)) + 
+#       geom_line(colour = "red") + 
+#       scale_y_continuous(limits = c(0, 1000)) + 
+#       labs(title = paste0(coc.list[i])) + 
+#       theme_classic()
+# }
+# 
+# 
+# # apply fn in loop: 
+# for (i in 1:5){
+#       plots[[i]] <- 
+#       deaths.data$stl.decomp[[i]] %>% 
+#       as.data.frame() %>% 
+#       plot.fn() 
+#       
+# }
+# 
+# # view plots: 
+# print(plots)
+# plots[[5]]
+# 
+# ggarrange(plots[[2]], 
+#           plots[[3]], 
+#           plots[[4]], 
+#           plots[[5]])
 
 
